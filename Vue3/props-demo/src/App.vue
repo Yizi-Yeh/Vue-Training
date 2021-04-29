8b385dd159c9d527fcf75368abdbe112c251603c
Yizi-Yeh <g384357@gmail.com>
2021-04-29 08:31:19 +0800

    元件間透過 props 傳遞屬性，子元件若要接受props屬性，需要透過 setup() 函式回傳props參數後，才能使用。

---
+++ Vue3/props-demo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<PropsTest :msg="data"/>
+</template>
+
+<script>
+import PropsTest from "@/components/PropsTest.vue"
+
+export default {
+components:{
+  PropsTest
+},
+setup(){
+  const data = ref("hello")
+  return {
+    data
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue3/props-demo/src/components/PropsTest.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- props是個物件，物件中的msg -->
+  <h1>{{props.msg}}</h1>
+</template>
+
+<script>
+export default {
+    props:['msg'],
+    // props必須透過 setup 函式回傳
+    setup(props){
+        console.log(props)
+        return{
+            props
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color:bisque
+}
+</style>
