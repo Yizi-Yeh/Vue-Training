23ad48bd6a52e154b83b95703398058eee13a3f4
Yizi-Yeh <g384357@gmail.com>
2021-04-29 09:11:43 +0800

    透過屬性傳遞資料時，要注意各種型別的預設設定，特別是 Array & Object (官方建議default值透過return回傳） default:()=>[] / default:()=>({})

--- Vue3/props-demo/src/components/PropsTest.vue
+++ Vue3/props-demo/src/components/PropsTest.vue
@@ -1,13 +1,39 @@
 <template>
 <!-- props是個物件，物件中的msg -->
-  <h1>{{props.msg}}</h1>
+  <h1>{{props.str}}</h1>
 </template>
 
 <script>
 export default {
     // 傳入 props 時，不建議以 array 方式傳入（未能明確告知型別）
     props:{
-        msg:String,
+        str:{
+            type:String,
+            default:"its String"
+        },
+        bool:{
+            type:Boolean,
+            default:ture
+        },
+        num:{
+            type:Number,
+            default:1
+        },
+        arr:{
+            // Array & Obj 在預設型別時，需要透過 return 回傳
+            type:Array,
+            // 利用函式返回
+            default:()=>[]
+        },
+        obj:{
+            // Array & Obj 在預設型別時，需要透過 return 回傳
+            type:Object,
+            default:()=>({})
+        },
+        handClickFn:{
+            type:Function,
+            default:()=>{}
+        }
     },
     // props必須透過 setup 函式回傳
     setup(props){
