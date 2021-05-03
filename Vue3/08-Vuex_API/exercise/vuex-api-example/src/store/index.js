bf51d45bf2b0626bd9459d1501154ff733cf8fb2
Yizi-Yeh <g384357@gmail.com>
2021-05-03 08:45:44 +0800

    在 mounted 階段 dispatch 抓取 store 中的 action 資料，再commit 至 mutation ，改變 state 中的狀態

--- Vue3/08-Vuex_API/exercise/vuex-api-example/src/store/index.js
+++ Vue3/08-Vuex_API/exercise/vuex-api-example/src/store/index.js
@@ -1,13 +1,26 @@
 import { createStore } from "vuex";
-
+import axios from 'axios'
 export default createStore({
   state: {
     photoArr: [],
     idx: 0,
     isLoad: false,
   },
-  actions: {},
-  mutations: {},
+  actions: {
+    // action 後的資料 commit 到 mutation，先將他解構出來
+    handInit({commit})
+    {
+      axios.get('https://vue-lessons-api.heroku.app.com/photo/list').then(res=>{
+        commit('init',res.data)
+      })
+
+    }
+  },
+  mutations: {
+    init(state,payload){
+      state.photoArr = payload
+    }
+  },
   getters: {
     isLoad(state) {
       return state.isLoad;
