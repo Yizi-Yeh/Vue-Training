4e8e2da913ae39bd4aec3b3090b7c1ee53747433
Yizi-Yeh <g384357@gmail.com>
2021-05-04 08:23:18 +0800

    在模板中將將需要被存取的元素，，加上ref屬性，並賦值，他就會被存在ref物件中，以此方式取得DOM元素  ref="textInput"

---
+++ Vue3/props-demo/src/components/TemplateRefs.vue
@@ -0,0 +1,31 @@
+<template>
+  <input ref="textInput" type="text" id="textVar" placeholder="input text">
+</template>
+
+<script>
+import {onMounted,ref} from 'vue'
+export default {
+setup(){
+
+    // 宣告的變數需和 ref屬性名稱相同
+    // 透過 ref方法 綁定在 DOM元素身上，透過此方式取得
+    const textInput = ref(null)
+    onMounted(()=>{
+    // document.getElementById('textBar').focus();
+    textInput.value.facus()
+    })
+    // 宣告後的變數 return
+    return{textInput}
+}
+}
+</script>
+
+<style scoped>
+input{
+    display: block;
+    width: 100%;
+    height: 30px;
+    background-color: beige;
+    padding-left: 10px;
+}
+</style>
