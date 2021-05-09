42f36f8306b13c773af50ee5a2b16b95219b50cf
Yizi-Yeh <g384357@gmail.com>
2021-05-09 17:16:05 +0800

    透過 state 中的 module 功能切分各功能，並在module檔中設定 namespaced:true

---
+++ Vue3/07-Vuex/exercise/vuex-example/src/store/getters.js
@@ -0,0 +1,7 @@
+export default {
+    getters: {
+        isOpen(state){
+          return state.isOpen
+        }
+      },
+}
--- Vue3/07-Vuex/exercise/vuex-example/src/store/index.js
+++ Vue3/07-Vuex/exercise/vuex-example/src/store/index.js
@@ -4,12 +4,15 @@ import actions from './actions.js'
 import mutations from './mutations'
 import getters from './getters'
 
+import Auth from './Auth/index'
+
 export default createStore({
   // 透過解構方式放到createStore中
   state,
   actions,
   mutations,
-  getters
-  // 透過getters 取得 state
-  // 先在getters處理資料
+  getters,
+  modules:{
+    Auth
+  }
 });
